f97c5707f713fde8569a562cd646394909cd329e
Anilkumar18 <anilkumarunni@gmail.com>
2020-12-04 13:45:10 +0530

    committed

--- deploy/increment.sh
+++ deploy/increment.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 
 # Get needed versions from a
-PLUGIN_CURRENT_VERSION=$(awk '/    Version/{print $NF}' lifterlms_enrollment_importer.php)
-PLUGIN_NEXT_VERSION=$(echo $PLUGIN_CURRENT_VERSION | awk -F. -v OFS=. 'NF==1{print ++$NF}; NF>1{$NF=sprintf("%0*d", length($NF), ($NF+1)); print}')
+THEME_CURRENT_VERSION=$(awk '/    Version/{print $NF}' style.css)
+THEME_NEXT_VERSION=$(echo $PLUGIN_CURRENT_VERSION | awk -F. -v OFS=. 'NF==1{print ++$NF}; NF>1{$NF=sprintf("%0*d", length($NF), ($NF+1)); print}')
 
 # Replace versions with new version
 # - In main file
-sed -i "" "s/${PLUGIN_CURRENT_VERSION}/${PLUGIN_NEXT_VERSION}/g" lifterlms_enrollment_importer.php
+sed -i "" "s/${PLUGIN_CURRENT_VERSION}/${THEME_NEXT_VERSION}/g" style.css
 # - In versin file
-echo "{\"version\": \"${PLUGIN_NEXT_VERSION}\"}" > version.json
+echo "{\"version\": \"${THEME_NEXT_VERSION}\"}" > version.json
 
-echo "New version: "$PLUGIN_NEXT_VERSION
+echo "New version: "$THEME_NEXT_VERSION
